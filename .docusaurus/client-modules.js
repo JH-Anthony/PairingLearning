fa924302b27e5ef6337093346aa225a933563060
xujiahao <xujiahao@guanmai.cn>
2022-04-26 23:01:34 +0800

    feat: init

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Users/xujiahao/Desktop/test/PairingLearning/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/xujiahao/Desktop/test/PairingLearning/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/xujiahao/Desktop/test/PairingLearning/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
+  require('/Users/xujiahao/Desktop/test/PairingLearning/src/css/custom.css'),
+];
